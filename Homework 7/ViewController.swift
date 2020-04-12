0aafa71736dd2c3c43f75b0c460872311f7d772f
Kai Marshall <k.marshall@setonhill.edu>
2020-04-12 16:26:59 -0400

    Initial Commit

---
+++ Homework 7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 7
+//
+//  Created by Kai Marshall on 4/12/20.
+//  Copyright © 2020 Kai Marshall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Homework 7Tests/Homework_7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Homework_7Tests.swift
+//  Homework 7Tests
+//
+//  Created by Kai Marshall on 4/12/20.
+//  Copyright © 2020 Kai Marshall. All rights reserved.
+//
+
+import XCTest
+@testable import Homework_7
+
+class Homework_7Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
