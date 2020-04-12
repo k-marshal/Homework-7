5ad3a9be3d071288cf2fc04352f101fe5fb31e9c
Kai Marshall <k.marshall@setonhill.edu>
2020-04-12 19:11:03 -0400

    Ready to grade

---
+++ Homework 7/GuiderClass.swift
@@ -0,0 +1,30 @@
+//
+//  GuiderClass.swift
+//  Homework 7
+//
+//  Created by Kai Marshall on 4/12/20.
+//  Copyright © 2020 Kai Marshall. All rights reserved.
+//
+
+import UIKit
+
+class GuiderClass: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Homework 7/ViewController.swift
+++ Homework 7/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+import CoreLocation
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    let locMan: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
+    let kshuLatitude: CLLocationDegrees = 40.311
+    let kshuLongtitude: CLLocationDegrees = -79.5567
+    
+    
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation:  CLLocation=locations[0]
+        NSLog("Something is happening")
+        
+        if newLocation.horizontalAccuracy  >= 0 {
+            let shu:CLLocation  = CLLocation(latitude: kshuLatitude, longitude: kshuLatitude)
+            let delta:CLLocationDistance = shu.distance(from: newLocation)
+            let miles:Double = (delta * 0.000621371)  + 0.5
+            
+            if miles < 3 {
+                distanceLabel.text = "Welcome to Seton Hill"
+            } else {
+                let commaDelimited: NumberFormatter = NumberFormatter()
+                commaDelimited.numberStyle = NumberFormatter.Style.decimal
+                
+                distanceLabel.text=commaDelimited.string(from: NSNumber(value: miles))!+"miles to Seton Hill"
+            }
+            func viewDidLoad() {
+                locMan.delegate = self
+                locMan.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+                locMan.distanceFilter = 1609;
+                locMan.requestWhenInUseAuthorization()
+                locMan.startUpdatingLocation()
+                startLocation = nil
+}
+            
+        
+        
 
 
 }
-
+}
+}
